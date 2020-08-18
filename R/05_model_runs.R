e508a80c8997a381e85729221a22afa67dad7992
diogosbr <diogosbr@gmail.com>
2020-08-18 09:33:41 -0300

    parallel optimization

--- R/05_model_runs.R
+++ R/05_model_runs.R
@@ -7,6 +7,7 @@ library(dplyr)
 library(raster)
 library(progress)
 library(modleR)
+library(foreach)
 
 # reading species data
 target_species <- read.csv("./data/target_species.csv", stringsAsFactors = FALSE) %>%
@@ -24,61 +25,87 @@ target_species <- gsub(x = target_species, pattern = " ", replacement = "_")
 wc <- list.files("./data/env_sel/present/", pattern = "tif$", full.names = TRUE) %>%
   stack()
 
+
+# loops for ENM ----------------------------------------------------------------
+
+# parallelism
+cl = snow::makeCluster(11, outfile = 'outputs/models/out.log')
+doSNOW::registerDoSNOW(cl)
+
 # creating progress bar
 pb <- progress_bar$new(
   format = "(:spin) [:bar] :percent :elapsed sp: :current",
   total = length(target_species), clear = FALSE)
 
-# loops for ENM ----------------------------------------------------------------
+# setting Progress Bar
+progress_number = 1:length(target_species)
+progress = function(n){pb$tick(tokens = list(sp = progress_number[n]))}
+opts <- list(progress =  progress)
 
 # setup data
-for(sp in target_species){
-  pb$tick()
+#for(sp in target_species){
+#pb$tick()
+temp <-
+  foreach(sp = target_species, .inorder = FALSE,
+          .packages = c("modleR", "progress"),
+          .options.snow = opts) %dopar% {
 
-  species_df <- clean_df[clean_df$species == sp, ] # getting only occurrences for this species
+            species_df <- clean_df[clean_df$species == sp, ] # getting only occurrences for this species
 
-  # choosing the type of partition depending on the number of records
-  partition_type <- ifelse(nrow(species_df > 50), "crossvalidation", "bootstrap")
+            # choosing the type of partition depending on the number of records
+            partition_type <- ifelse(nrow(species_df) > 50, "crossvalidation", "bootstrap")
 
-  setup_sdmdata(species_name = sp,
-                occurrences = species_df,
-                predictors = wc, # set of predictors for running the models
-                models_dir = "./outputs/models", # folder to save partitions
-                seed = 123, # set seed for random generation of pseudoabsences
-                buffer_type = "maximum", # buffer type for sampling pseudoabsences, see help for types
-                clean_dupl = TRUE, # remove duplicate records
-                clean_nas = TRUE, # remove records with na values from variables
-                clean_uni = TRUE, # remove records falling at the same pixel
-                png_sdmdata = TRUE, # save minimaps in png
-                n_back = 10000, # number of pseudoabsences
-                partition_type = partition_type,
-                cv_partitions = 10, # number of folds for crossvalidation
-                cv_n = 1,# number of crossvalidation runs
-                boot_n = 10, # number of crossvalidation runs
-                boot_proportion = 0.1) # number of partitions in the crossvalidation
-}
+            setup_sdmdata(species_name = sp,
+                          occurrences = species_df,
+                          predictors = wc, # set of predictors for running the models
+                          models_dir = "./outputs/models", # folder to save partitions
+                          seed = 123, # set seed for random generation of pseudoabsences
+                          buffer_type = NULL, # buffer type for sampling pseudoabsences
+
+                          # env_filter = TRUE,
+                          # env_distance = "centroid",
+                          # min_env_dist = 0.1,
+
+                          clean_dupl = TRUE, # remove duplicate records
+                          clean_nas = TRUE, # remove records with na values from variables
+                          clean_uni = TRUE, # remove records falling at the same pixel
+                          png_sdmdata = TRUE, # save minimaps in png
+                          n_back = nrow(species_df) * 10, # number of pseudoabsences
+                          partition_type = partition_type,
+                          cv_partitions = 10, # number of folds for crossvalidation
+                          cv_n = 1,# number of crossvalidation runs
+                          boot_n = 10, # number of crossvalidation runs
+                          boot_proportion = 0.1) # number of partitions in the crossvalidation
+          }
+
+snow::stopCluster(cl)
 
 # partitions
-for(sp in target_species){
-  pb$tick()
+temp <-
+  foreach::foreach(sp = target_species, .inorder = FALSE,
+                   .packages = c("modleR", "progress"),
+                   .options.snow = opts) %dopar% {
+                     # for(sp in target_species){
+                     #   pb$tick()
 
-  # run selected algorithms for each partition
-  do_many(species_name = sp,
-          predictors = wc,
-          models_dir = "./outputs/models",
-          project_model = TRUE, # project models into other sets of variables
-          proj_data_folder = "./data/env_sel/future/", # folder with projection variables
-          #mask = ma_mask, # mask for projecting the models
-          png_partitions = TRUE, # save minimaps in png?
-          write_bin_cut = TRUE, # save binary and cut outputs?
-          dismo_threshold = "spec_sens", # threshold rule for binary outputs
-          equalize = TRUE, # equalize numbers of presence and pseudoabsences for random forest
-          bioclim = TRUE,
-          glm = TRUE,
-          maxent = TRUE,
-          rf = TRUE,
-          svmk = TRUE)
-}
+                     # run selected algorithms for each partition
+                     do_many(species_name = sp,
+                             predictors = wc,
+                             models_dir = "./outputs/models",
+                             project_model = TRUE, # project models into other sets of variables
+                             proj_data_folder = "./data/env_sel/future/", # folder with projection variables
+                             #mask = ma_mask, # mask for projecting the models
+                             png_partitions = TRUE, # save minimaps in png?
+                             write_bin_cut = TRUE, # save binary and cut outputs?
+                             dismo_threshold = "spec_sens", # threshold rule for binary outputs
+                             equalize = TRUE, # equalize numbers of presence and pseudoabsences for random forest
+                             bioclim = TRUE,
+                             glm = TRUE,
+                             maxent = TRUE,
+                             rf = TRUE,
+                             svmk = TRUE)
+                   }
+snow::stopCluster(cl)
 
 # final_models
 for(sp in target_species){
